deb504291a688a4a5561a0565da33bfb00b34dd3
Craig Andrews <candrews@integralblue.com>
2018-05-24 11:01:59 -0400

    The @SpringBootApplication should extend SpringBootServletInitializer

--- src/main/java/com/integralblue/transparentrouter/TransparentRouterApplication.java
+++ src/main/java/com/integralblue/transparentrouter/TransparentRouterApplication.java
@@ -2,6 +2,7 @@ package com.integralblue.transparentrouter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 /**
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @SuppressWarnings({"checkstyle:hideutilityclassconstructor", "PMD.UseUtilityClass"})
 @PropertySource(value = "file:./application-local.properties", ignoreResourceNotFound = true)
-public class TransparentRouterApplication {
+public class TransparentRouterApplication extends SpringBootServletInitializer {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(TransparentRouterApplication.class, args);
